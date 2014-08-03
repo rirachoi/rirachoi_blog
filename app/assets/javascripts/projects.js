d9bffd5a64f39c1cd6859583ad72bd38800a86d5
Rira Choi <rira.nunu.choi@gmail.com>
2014-08-03 23:56:40 +1000

    project preview img and css

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -11,11 +11,6 @@ $(document).ready(function(){
             '-webkit-transform':'rotate('+r+'deg)',
             'transform':'rotate('+r+'deg)'
             });
-            // $('#content').css({
-            //     '-moz-transform':'rotate('+5+'deg)',
-            //     '-webkit-transform':'rotate('+5+'deg)',
-            //     'transform':'rotate('+5+'deg)'
-            //     });
 
         // main_images animation
         $this.stop().animate({
@@ -26,42 +21,59 @@ $(document).ready(function(){
   }; //end showProjectPreview
 
   $('#content').hide();
+//////calling project preview
   showProjectPreview();
 
-/////////////// imgs hover options
-  $('.best_homeworks > img').hover(
+////////hover image preview
+imagePreview = function(){
+    xOffset = -20;
+    yOffset = 40;
 
-      function () {
-          var $this = $(this);
-          $('img',$this).stop().animate({
-              'marginTop':'-40px'
-          },200);
-      },
-      function () {
-          var $this = $(this);
-          $('a',$this).stop().animate({
-              'marginTop':'-70px'
-          },200);
-      }
-  ).click(function(){
+    $("a#preview").hover(function(e){
+        this.t = this.title;
+        this.title = "";
+       var c = (this.t != "") ? "<br/>" + this.t : "";
+         $("#content").append("<p id='preview_img'><img src='"+ this.href +"' alt='Image preview'/>"+ c +"</p>");
+         $("#preview_img")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px")
+            .fadeIn('slow').addClass('boxAnimation-target');
+    },
+
+    function(){
+        this.title = this.t;
+        $("#preview_img").remove();
+
+    });
+
+    $("a#preview").mousemove(function(e){
+        $("#preview_img")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px");
+    });
+};// end of preview image
+
+/////////////// main_imgs click
+  $('.best_homeworks > img').on('click', function(){
       $('#content').show();
 
-      var $this = $(this);
       var name = this.className;
-      console.log(name)
 
       $('#content').animate({marginTop:-100}, 1000,function(){
-          var $this = $(this);
-          var r=Math.floor(Math.random()*41)-20;
-          // $this.css({
-          //     '-moz-transform':'rotate('+r+'deg)',
-          //     '-webkit-transform':'rotate('+r+'deg)',
-          //     'transform':'rotate('+r+'deg)'
-          //     });
-          $('#content div').hide();
-          $('#'+name).show();
-          $this.animate({marginTop:100}, 500);
+        var $this = $(this);
+
+        $('#content div').hide();
+        $('#'+name).show();
+        imagePreview();
+    ///wtf
+          // $('.images').on('mouseover', function(){
+          //   var $this = $(this);
+          //   $this.toggleClass('mainIMG');
+          // });
+
+        $this.animate({marginTop:100}, 500);
       })
   }); //end click
+
 });//end document
 
