a9e4eef5d652f380c22159ceb311591e1033512f
Rira Choi <rira.nunu.choi@gmail.com>
2014-08-08 09:59:40 +1000

    project button top

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -83,6 +83,9 @@ $(document).ready(function(){
 
   }); //end of main image click
 
+  $('.btn_top').on('click', function(){
+    $("html, body").animate({ scrollTop: 0 }, 'slow');
+  });
 
 });//end document
 
